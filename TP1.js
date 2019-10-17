e8a9ae301b9ae717e2b384ddff7b1bee3af23864
Alain Roger SANGA <ar.sanga@sciencesu-lyon.fr>
2019-10-17 11:50:08 +0200

    operations

--- TP1.js
+++ TP1.js
@@ -1,2 +1,30 @@
-let test = 'Bonjour';
-console.log(test);
+console.log('*****Application opérateurs*******');
+
+const readline = require('readline');
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+
+function ask(questionText) {
+    return new Promise((resolve, reject) => {
+        readlineInterface.question(questionText, resolve);
+    });
+}
+
+
+start();
+
+async function start() {
+    let number1BP = await ask('Premier nombre...? ');
+    let number2BP = await ask('Dexieme nombre ....? ');
+    let number1 = parseInt(number1BP.toString());
+    let number2 = parseInt(number2BP.toString());
+    let somme = number1 + number2;
+    let substract  = number1 - number2;
+    let mult = number1 * number2;
+    let division = number1 / number2;
+
+    console.log('La somme est ;', somme);
+    console.log('La soustraction est ;', substract);
+    console.log('La somme multiplication est : ;', mult);
+    console.log('La division est ;', division);
+    process.exit();
+}
